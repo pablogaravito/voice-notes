3f42744406a287d26d089abeb6ef6c3ce5c3d2de
Pablo <pablo.garavito@gmail.com>
2025-06-13 00:48:19 -0500

    reworked visuals, added drag container for file input

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -16,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const downloadBothTextButton = document.getElementById('downloadBothText');
   const themeToggle = document.getElementById('themeToggle');
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+  const uploadContainer = document.getElementById('uploadContainer');
 
   const DARK_ICON = `
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
